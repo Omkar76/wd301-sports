7c7814cb03df91bb390750a0af47fc48f5da5825
Onkar Jadhav <omjadhav2610@gmail.com>
2023-09-13 20:16:31 +0530

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,37 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate', devOptions: {
-      enabled: true
-    } })
+    VitePWA({
+      registerType: 'prompt', 
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Sportify',
+        short_name: 'Sportify',
+        theme_color: '#38a169',
+        description: 'Experience sports like never before with our web app! Stay in the know with the latest sports news and live scores. Catch every game-changing moment, from touchdowns to goals, all in one place. Get your sports fix now!',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 });
